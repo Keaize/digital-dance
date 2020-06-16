fd0d074d810357cc1c6eebc2a94420fa5e247bfc
hayoreo <nimjero@gmail.com>
2020-06-16 14:36:23 -0700

    Changed background video to something a little smaller, reducing theme size to <20mbs.

--- BGAnimations/ScreenSelectMusic background/default.lua
+++ BGAnimations/ScreenSelectMusic background/default.lua
@@ -1,7 +1,7 @@
 local af = Def.ActorFrame{
 
  LoadActor("./background.mp4")..{
-	InitCommand=function(self) self:diffusealpha(0.3):valign(0):y(0):loop(true) end,
+	InitCommand=function(self) self:diffusealpha(0.18):FullScreen():valign(0):y(0):loop(true) end,
 }
 }
 return af
